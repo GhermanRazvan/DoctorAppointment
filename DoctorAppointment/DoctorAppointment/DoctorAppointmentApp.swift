f582c9c4c908af0450626484f0054821a31d69a7
Razvan Gherman <razvan@192-168-0-201.rdsnet.ro>
2023-04-03 16:00:37 +0300

    First commit

---
+++ DoctorAppointment/DoctorAppointment/DoctorAppointmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoctorAppointmentApp.swift
+//  DoctorAppointment
+//
+//  Created by Razvan Gherman on 03.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct DoctorAppointmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
